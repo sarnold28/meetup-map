d195a383cbbfb88e2302ef156af26659984f08ba
sarnold28 <51327430+sarnold28@users.noreply.github.com>
2021-07-06 11:14:37 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,54 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
-WA.openCoWebSite('https://workadventu.re');
+// WA.openCoWebSite('https://workadventu.re');
+let currentPopup: any = undefined
+const firstPopupText = 'Congratulations, you found a hidden item in them map. Welcome to AIR:Leben'
+const secondPopupText = 'Congratulations, you found a hidden item in them map.'
+
+WA.onEnterZone('firstPopup', () => {
+    currentPopup = WA.openPopup("firstPopup", firstPopupText, [
+        {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }
+]);
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('firstPopup', () => {
+    // currentPopup.close();
+    closePopUp()
+});
+
+WA.onEnterZone('secondPopup', () => {
+    currentPopup = WA.openPopup("secondPopup", secondPopupText, [
+        {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }
+]);
+});
+
+// Close the popup when we leave the zone.
+WA.onLeaveZone('secondPopup', () => {
+    // currentPopup.close();
+    closePopUp()
+});
+
+
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
